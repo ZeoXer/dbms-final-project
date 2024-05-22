246a1488742ea82519c90cf0eb3eef1c09bd1b4a
ZeoXer <andrew17413@gmail.com>
2024-05-22 18:05:27 +0800

    add backend basic structure

---
+++ backend/__init__.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import Item
---
+++ backend/models.py
@@ -0,0 +1,10 @@
+from app import db
+
+
+class Item(db.Model):
+    __tablename__ = "Item"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f'<Item {self.name}>'
