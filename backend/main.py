68348ebffc724e41ac5c4af9307c292508fa2b67
HCC0711 <153408695+HCC0711@users.noreply.github.com>
2024-06-24 20:02:13 +0800

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
-from __init__ import app, db
+from __init__ import app, db, register_blueprints
+
+with app.app_context():
+    try:
+        db.create_all()
+        print("資料庫連接成功")
+    except Exception as e:
+        print("資料庫連接失敗", e)
+
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
+        register_blueprints(app)
     app.run(port=5432, debug=True)
