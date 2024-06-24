5fe2b37a26d8431e6258765d58476b423d5e6521
HCC0711 <153408695+HCC0711@users.noreply.github.com>
2024-06-24 20:16:53 +0800

    Add files via upload

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flasgger import Swagger
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,31 +9,14 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-swagger_config = {
-    "headers": [],
-    "specs": [{
-        "endpoint": "apispec_1",
-        "route": "/apispec_1.json",
-        "rule_filter": lambda rule: True,
-        "model_filter": lambda tag: True,
-    }],
-    "static_url_path":
-    "/flasgger_static",
-    "swagger_ui":
-    True,
-    "specs_route":
-    "/swagger/",
-    "title":
-    "教師履歷管理系統 API 文件",
-}
-swagger = Swagger(app, config=swagger_config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from models import Department, Teacher
-from api.departmentView import departmentView
-from api.teacherView import teacherView
-
-app.register_blueprint(departmentView, url_prefix='/departmentView')
-app.register_blueprint(teacherView, url_prefix='/teacherView')
+def register_blueprints(app):
+    from api import api
+    from teacherView import teacherView
+    from departmentView import departmentView
+    app.register_blueprint(api)
+    app.register_blueprint(teacherView, url_prefix='/teacherView')
+    app.register_blueprint(departmentView, url_prefix='/departmentView')
